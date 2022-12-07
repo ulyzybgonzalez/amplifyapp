d518ed4fe7eac7544179ab2d11c8322924f05d5a
Fotia Polemistis <119896287+ulyzybgonzalez@users.noreply.github.com>
2022-12-07 16:01:16 -0600

    App.js file updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Front End Application
         </a>
       </header>
     </div>
